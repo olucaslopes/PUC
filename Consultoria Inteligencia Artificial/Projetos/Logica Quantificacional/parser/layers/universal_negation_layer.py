aa0078999610143ad7753a7f1956005fe81e432a
GuTessitore <gugatessi@gmail.com>
2023-11-25 12:53:39 -0300

    refactor: separate logic in to layers

---
+++ Consultoria Inteligencia Artificial/Projetos/Logica Quantificacional/parser/layers/universal_negation_layer.py
@@ -0,0 +1,15 @@
+from parser.layers.abstract_layer import Layer
+
+
+class UniversalNegationLayer(Layer):
+    def __init__(self, parser):
+        super().__init__(parser)
+
+    def apply_rule(self):
+        if self.first_token == 'nenhum':
+            lemma_suj, lemma_pred = self.separate_subject_predicate_and_lemmatize()
+            if self.is_negation:
+                return f'¬exists x.({lemma_suj}(x) & ¬{lemma_pred}(x))'
+            else:
+                return f'¬exists x.({lemma_suj}(x) & {lemma_pred}(x))'
+        return self.sentence
