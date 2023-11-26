2c238e65460bfb9b4394b6d59530e4d6f009930d
Lucas Lopes Amorim <lucaslopesamorim@gmail.com>
2023-11-26 14:42:02 -0300

    feat: add function get_logic_phrases

--- Consultoria Inteligencia Artificial/Projetos/Logica Quantificacional/phrase_parser/knowledge_base.py
+++ Consultoria Inteligencia Artificial/Projetos/Logica Quantificacional/phrase_parser/knowledge_base.py
@@ -5,9 +5,20 @@ from nltk.inference.tableau import TableauProver, Expression
 class KnowledgeBase:
     def __init__(self):
         self.beliefs = {}
+        self.generated_logic_phrases = False
+        self.logic_phrases = list()
 
     def add_belief(self, phrase, logic_phrase):
+        """
+        TODO: Use Tableau inside this function to check if you are able to add a belief
+        """
+        self.generated_logic_phrases = False
         self.beliefs[phrase] = logic_phrase
+
+    def get_logic_phrases(self):
+        self.logic_phrases = [Expression.fromstring(logic) for logic in self.beliefs.values()]
+        self.generated_logic_phrases = True
+        return self.logic_phrases
 
     def check_belief(self, phrase):
         logic_phrase = str(SentenceParser(phrase).parse())
