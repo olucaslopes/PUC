541c5c5d3875a8ddc2de65e5f8155de7eeadb0e5
Isaac Higuchi <isaac_higuchi@outlook.com>
2023-11-26 12:36:45 -0300

    add main.py

---
+++ Consultoria Inteligencia Artificial/Projetos/Logica Quantificacional/main.py
@@ -0,0 +1,49 @@
+from phrase_parser.knowledge_base import KnowledgeBase
+
+def main():
+    knowledge_base = KnowledgeBase()
+
+    while True:
+        phrase = input("Digite uma frase: ")
+        print("Frase digitada: ", phrase)
+
+        tautology, contradition, redundant, consistant, logic_phrase = knowledge_base.check_belief(phrase)
+
+        print("Testando a frase...")
+        #quando a variavel for True  = Reprovado / False = Aprovado
+        if tautology:
+            print("Tautologia: Aprovado!")
+        else:
+            print("Tautologia: Reprovado!")
+
+        if contradition:
+            print("Contradição: Aprovado!")
+        else:
+            print("Contradição: Reprovado!")
+
+        if redundant:
+            print("Redundância: Aprovado!")
+        else:
+            print("Redundância: Reprovado!")
+
+        if consistant:
+            print("Consistência: Aprovado!")
+        else:
+            print("Consistência: Reprovado!")
+
+        if not (tautology or contradition or redundant or consistant):
+            print("A frase é válida!")
+            print("Adicionando à base de crenças...")
+            knowledge_base.add_belief(phrase, logic_phrase)
+            print("Base de crenças atualizada: ", knowledge_base.base_crencas)
+        else:
+            print("A frase não é válida!")
+            print("Base de crenças atual: ", knowledge_base.base_crencas)
+
+        continuar = input("Quer digitar uma nova frase? (S/N): ")
+        if continuar.upper() != "S":
+            print("Fim!")
+            break
+
+if __name__ == "__main__":
+    main()
