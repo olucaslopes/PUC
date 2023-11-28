d0b8268d65a9e819fb8873f8b8bfaead48905e81
Lucas Lopes Amorim <lucaslopesamorim@gmail.com>
2023-11-28 09:41:33 -0300

    fix: negative sign

--- Consultoria Inteligencia Artificial/Projetos/Logica Quantificacional/phrase_parser/test/test_parser.py
+++ Consultoria Inteligencia Artificial/Projetos/Logica Quantificacional/phrase_parser/test/test_parser.py
@@ -5,13 +5,13 @@ from phrase_parser.sentence_parser import SentenceParser
 
 @pytest.mark.parametrize("phrase, expected", [
     ("Sócrates é homem", "homem(socrates)"),
-    ("Raul não é cientista.", "¬cientista(raul)"),
+    ("Raul não é cientista.", "-cientista(raul)"),
     ("Todos os homens são mortais.", "all x.(homem(x) -> mortal(x))"),
-    ("Todos os homens não são répteis.", "¬exists x.(homem(x) & reptel(x))"),
+    ("Todos os homens não são répteis.", "-exists x.(homem(x) & reptel(x))"),
     ("Algum homem é mortal", "exists x.(homem(x) & mortal(x))"),
-    ("Algum homem não é palmeirense.", "exists x.(homem(x) & ¬palmeirense(x))"),
-    ("Nenhum homem é réptil.", "¬exists x.(homem(x) & reptil(x))"),
-    ("Nenhum homem não é humano.", "¬exists x.(homem(x) & ¬humano(x))")
+    ("Algum homem não é palmeirense.", "exists x.(homem(x) & -palmeirense(x))"),
+    ("Nenhum homem é réptil.", "-exists x.(homem(x) & reptil(x))"),
+    ("Nenhum homem não é humano.", "-exists x.(homem(x) & -humano(x))")
 ])
 def test_sentence_parsing(phrase, expected):
     parser = SentenceParser(phrase)
