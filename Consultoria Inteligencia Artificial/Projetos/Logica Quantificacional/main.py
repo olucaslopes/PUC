14c135688a16fa9e48e32d81c4b3cafbf2e61d89
Lucas Lopes Amorim <lucaslopesamorim@gmail.com>
2023-11-27 23:05:12 -0300

    refactor: refactor main file

--- Consultoria Inteligencia Artificial/Projetos/Logica Quantificacional/main.py
+++ Consultoria Inteligencia Artificial/Projetos/Logica Quantificacional/main.py
@@ -1,49 +1,25 @@
 from phrase_parser.knowledge_base import KnowledgeBase
+from phrase_parser.sentence_parser import SentenceParser
 
 def main():
     knowledge_base = KnowledgeBase()
 
     while True:
-        phrase = input("Digite uma frase: ")
-        print("Frase digitada: ", phrase)
-
-        tautology, contradition, redundant, consistant, logic_phrase = knowledge_base.check_belief(phrase)
-
-        print("Testando a frase...")
-        #quando a variavel for True  = Reprovado / False = Aprovado
-        if tautology:
-            print("Tautologia: Aprovado!")
-        else:
-            print("Tautologia: Reprovado!")
-
-        if contradition:
-            print("Contradição: Aprovado!")
-        else:
-            print("Contradição: Reprovado!")
-
-        if redundant:
-            print("Redundância: Aprovado!")
-        else:
-            print("Redundância: Reprovado!")
-
-        if consistant:
-            print("Consistência: Aprovado!")
-        else:
-            print("Consistência: Reprovado!")
-
-        if not (tautology or contradition or redundant or consistant):
-            print("A frase é válida!")
-            print("Adicionando à base de crenças...")
-            knowledge_base.add_belief(phrase, logic_phrase)
-            print("Base de crenças atualizada: ", knowledge_base.beliefs)
-        else:
-            print("A frase não é válida!")
-            print("Base de crenças atual: ", knowledge_base.beliefs)
-
-        continuar = input("Quer digitar uma nova frase? (S/N): ")
-        if continuar.upper() != "S":
-            print("Fim!")
+        # Obtendo input de uma crença
+        new_belief = input('Insira uma nova crença ou a palavra \'sair\' para terminar: ')
+        if new_belief.lower().strip() == 'sair':
             break
+
+        # Transformando frase do português para expressão do nltk
+        parser = SentenceParser(new_belief)
+        logic_belief = str(parser.parse())
+
+        print("Expressão lógica: ", logic_belief)
+
+        print("Testando a frase e adicionado na base de crenças...")
+
+        # Faz 4 testes e se sim adiciona na base de crenças
+        knowledge_base.add_belief(new_belief, logic_belief)
 
 if __name__ == "__main__":
     main()
